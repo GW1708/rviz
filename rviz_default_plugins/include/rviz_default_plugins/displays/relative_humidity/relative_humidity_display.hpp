e9aa3b152a30cebf60e0ac371bba04da50554adc
Maximilian Kuehn <maximilian.kuehn@tngtech.com>
2018-11-19 11:13:23 +0100

    First changes for compilation to be successful

---
+++ rviz_default_plugins/include/rviz_default_plugins/displays/relative_humidity/relative_humidity_display.hpp
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2008, Willow Garage, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Willow Garage, Inc. nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef RVIZ_DEFAULT_PLUGINS__DISPLAYS__RELATIVE_HUMIDITY__RELATIVE_HUMIDITY_DISPLAY_HPP_
+#define RVIZ_DEFAULT_PLUGINS__DISPLAYS__RELATIVE_HUMIDITY__RELATIVE_HUMIDITY_DISPLAY_HPP_
+
+#include <memory>
+
+#include "sensor_msgs/msg/point_cloud2.hpp"
+#include "sensor_msgs/msg/relative_humidity.hpp"
+
+#include "rviz_common/ros_topic_display.hpp"
+#include "rviz_common/properties/queue_size_property.hpp"
+#include "rviz_default_plugins/displays/pointcloud/point_cloud_common.hpp"
+
+#include "rviz_default_plugins/visibility_control.hpp"
+
+namespace rviz_common
+{
+    namespace properties
+    {
+        class IntProperty;
+    }    // namespace properties
+}  // namespace rviz_common
+
+namespace rviz_default_plugins
+{
+
+    class PointCloudCommon;
+
+    namespace displays
+    {
+
+/**
+ * \class
+ * \brief Displays a RelativeHumidity message of type sensor_msgs::RelativeHumidity
+ *
+ */
+
+        class RVIZ_DEFAULT_PLUGINS_PUBLIC RelativeHumidityDisplay : public
+                                                                 rviz_common::RosTopicDisplay<sensor_msgs::msg::RelativeHumidity>
+        {
+        Q_OBJECT
+
+        public:
+            RelativeHumidityDisplay();
+            ~RelativeHumidityDisplay();
+
+            void reset() override;
+            void update(float wall_dt, float ros_dt) override;
+            void onDisable() override;
+            void processMessage(const sensor_msgs::msg::RelativeHumidity::ConstSharedPtr msg) override;
+
+        protected:
+            void onInitialize() override;
+
+        private:
+            std::unique_ptr<rviz_common::QueueSizeProperty> queue_size_property_;
+            std::shared_ptr<PointCloudCommon> point_cloud_common_;
+        };
+
+    }  // namespace displays
+}  // namespace rviz_default_plugins
+
+#endif // RVIZ_DEFAULT_PLUGINS__DISPLAYS__RELATIVE_HUMIDITY__RELATIVE_HUMIDITY_DISPLAY_HPP_
