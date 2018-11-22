eefb04f0c573d7f10bc7ebf1a0e27cac2768db57
Maximilian Kuehn <maximilian.kuehn@tngtech.com>
2018-11-22 11:10:41 +0100

    Code Review #1
    Fixed Minor Things and Refurbished mainly process Message Function.
    Split it up in three different functions for easier readability.
    Split up also allows to unit test it now because point cloud common
    does not have to be initialised

--- rviz_default_plugins/test/rviz_default_plugins/publishers/temperature_publisher.hpp
+++ rviz_default_plugins/test/rviz_default_plugins/publishers/temperature_publisher.hpp
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2017, Bosch Software Innovations GmbH.
- * Copyright (c) 2018, Maximilian Kuehn
+ * Copyright (c) 2018, TNG Technology Consulting GmbH.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -32,9 +32,10 @@
 #define RVIZ_DEFAULT_PLUGINS__PUBLISHERS__TEMPERATURE_PUBLISHER_HPP_
 
 #include <string>
-
 #include <chrono>
+
 #include "rclcpp/rclcpp.hpp"
+
 #include "sensor_msgs/msg/temperature.hpp"
 #include "std_msgs/msg/header.hpp"
 
@@ -45,35 +46,31 @@ using namespace std::chrono_literals; //NOLINT
 namespace nodes
 {
 
-sensor_msgs::msg::Temperature createTemperature(float temp, float var)
+sensor_msgs::msg::Temperature createTemperatureMessage(float temperature, float variance)
 {
-  sensor_msgs::msg::Temperature temperature;
-  temperature.temperature = temp;
-  temperature.variance = var;
+  sensor_msgs::msg::Temperature temperatureMessage;
 
-  return temperature;
+  temperatureMessage.header = std_msgs::msg::Header();
+  temperatureMessage.header.frame_id = "temperature_frame";
+  temperatureMessage.header.stamp = rclcpp::Clock().now();
+
+  temperatureMessage.temperature = temperature;
+  temperatureMessage.variance = variance;
+
+  return temperatureMessage;
 }
 
 class TemperaturePublisher : public rclcpp::Node
 {
 public:
   TemperaturePublisher()
-  : Node("temperature_publisher"),
-    count_(0)
+  : Node("temperature_publisher")
   {
     publisher_ = this->create_publisher<sensor_msgs::msg::Temperature>("temperature");
 
     auto timer_callback =
       [this]() -> void {
-        auto message = sensor_msgs::msg::Temperature();
-
-        message.header = std_msgs::msg::Header();
-        message.header.frame_id = "temperature_frame";
-        message.header.stamp = rclcpp::Clock().now();
-
-        message.temperature = 20.0;
-        message.variance = 1.0;
-
+        auto message = createTemperatureMessage(20., 1.);
         this->publisher_->publish(message);
       };
     timer_ = this->create_wall_timer(500ms, timer_callback);
@@ -82,7 +79,6 @@ public:
 private:
   rclcpp::TimerBase::SharedPtr timer_;
   rclcpp::Publisher<sensor_msgs::msg::Temperature>::SharedPtr publisher_;
-  size_t count_;
 };
 
 }  // namespace nodes
